5a555b4978648cfa96dfd02eb44305cb2e7ec3f6
David Pustka <134135989+dr0th3r@users.noreply.github.com>
2023-12-07 16:11:17 +0000

    Add card flipping animation

--- src/lib/components/Card.svelte
+++ src/lib/components/Card.svelte
@@ -6,58 +6,57 @@
   //console.log(imgUrl);
 </script>
 
-<div class="outer">
-    {#if !found}
-      <button on:click disabled={flipped} >
-        <img src={imgUrl} alt="test" />
-      </button>
-    {:else}
-      <p>found</p>
-    {/if}
-</div>
+<button
+  on:click
+  disabled = {flipped || found}
+  class="card"
+  class:flipped = {flipped || found}
+>
+  <div 
+    class="img-container"
+    class:found
+  >
+    <img src={imgUrl} alt="card"/>
+  </div>
+</button>
 
 <style>
-  div,
-  button {
+  .card {
     height: 150px;
     width: 150px;
-    border-radius: 20px;
-  }
-
-  div {
-    background-color: rgba(255, 255, 255, 0.1);
-    color: white;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-  }
-
-  .outer, button {
     cursor: pointer;
+    background-color: #222;
+    border: none;
+    border-radius: 20px;
+    rotate: y 0deg;
+    transition: rotate 0.3s ease-out;
+    transform-style: preserve-3d;
     border: 2px solid rgba(255, 255, 255, 0.65);
+
+    &.flipped {
+      rotate: y 180deg;
+    }
   }
 
-  button,
-  button:focus,
-  button:active {
-    border: none;
-    background: none;
-    outline: none;
-    padding: 0;
-    background-color: transparent;
+  .img-container {
+    position: absolute;
+    inset: 0;
     display: flex;
     justify-content: center;
     align-items: center;
+    backface-visibility: hidden;
+    rotate: y 180deg;
+
+    &.found {
+      opacity: 0.6;
+      cursor: initial;
+    }
   }
 
   img {
+    width: 130px;
+    height: 130px;
     -webkit-user-drag: none;
-    -khtml-user-drag: none;
-    -moz-user-drag: none;
-    -o-user-drag: none;
-    user-drag: none;
-    width: 90%;
-    height: 90%;
   }
 
 </style>
