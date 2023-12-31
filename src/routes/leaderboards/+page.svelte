33aba588139a9db33771f5c3acf1f5ce2a9f1668
dr0th3r <dr0th3r.code@gmail.com>
2023-12-31 17:43:13 +0100

    Add sorting leaderboards

--- src/routes/leaderboards/+page.svelte
+++ src/routes/leaderboards/+page.svelte
@@ -1,35 +1,73 @@
+<script>
+  let users = [
+    {
+      username: "Cupomaz",
+      pexesosSolved: 7,
+      leastMoves: 13,
+      mostPairsFoundInRow: 4,
+      country: "DE",
+    },
+    {
+      username: "dr0th3r",
+      pexesosSolved: 32,
+      leastMoves: 10,
+      mostPairsFoundInRow: 6,
+      country: "CZ",
+    },
+    {
+      username: "government",
+      pexesosSolved: 16,
+      leastMoves: 15,
+      mostPairsFoundInRow: 5,
+      country: "UG",
+    },
+  ];
+
+  $: categories = Object.keys(users[0]);
+
+  let sortCategory = "pexesosSolved";
+  let isDescending = true;
+
+  function handleFilter(category) {
+    if (category === sortCategory) {
+      isDescending = !isDescending;
+    } else {
+      sortCategory = category;
+      isDescending = true;
+    }
+  }
+
+  function camelToNormal(str) {
+    return str
+      .replace(/([A-Z])/g, " $1")
+      .replace(/^./, (str) => str.toUpperCase());
+  }
+</script>
+
 <main>
   <div class="table-container">
     <table>
       <thead>
         <tr>
-          <th>Username</th>
-          <th>Pexeso's solved</th>
-          <th>Least moves</th>
-          <th>Most pairs found in a row</th>
-          <th>Country</th>
+          {#each categories as category}
+            <th on:click={() => handleFilter(category)}>
+              <span class="outer-span">
+                {@html `${camelToNormal(category)}&nbsp;${
+                  category === sortCategory ? (isDescending ? "▼" : "▲") : ""
+                }`}
+              </span>
+            </th>
+          {/each}
         </tr>
-        <tr>
-          <td>Cupomaz</td>
-          <td>7</td>
-          <td>13</td>
-          <td>4</td>
-          <td>DE</td>
-        </tr>
-        <tr>
-          <td>dr0th3r</td>
-          <td>32</td>
-          <td>10</td>
-          <td>6</td>
-          <td>CZ</td>
-        </tr>
-        <tr>
-          <td>government</td>
-          <td>16</td>
-          <td>15</td>
-          <td>5</td>
-          <td>UG</td>
-        </tr>
+        {#each users.sort((a, b) => (a[sortCategory] > b[sortCategory] ? 1 : -1) * (isDescending ? -1 : 1)) as user}
+          <tr>
+            <td>{user.username}</td>
+            <td>{user.pexesosSolved}</td>
+            <td>{user.leastMoves}</td>
+            <td>{user.mostPairsFoundInRow}</td>
+            <td>{user.country}</td>
+          </tr>
+        {/each}
       </thead>
     </table>
   </div>
@@ -41,8 +79,6 @@
     display: flex;
     justify-content: center;
     align-items: center;
-    width: 100wh;
-    height: 100vh;
     flex-direction: column;
     background-color: #111827;
   }
@@ -52,7 +88,7 @@
     width: clamp(
       250px,
       90vw,
-      930px
+      90vw
     ); /* 600px for the table + 30 px for the padding*/
     overflow-x: auto;
     padding: 15px;
@@ -68,6 +104,9 @@
   th {
     font-size: 1.4rem;
     text-align: left;
+    user-select: none;
+    cursor: pointer;
+    max-width: 200px;
   }
 
   td {
